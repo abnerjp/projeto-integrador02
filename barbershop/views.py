6b416d2cbadd6d80336183e9b3a2a4b557c5236a
Abner J Pelisser <abner.pelisser@gmail.com>
2022-06-01 01:39:13 -0300

    pagina inicial criada. ajustando pagina inicial

--- barbershop/views.py
+++ barbershop/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Página inicial")
-    # return render(request, '')
+    return render(request, 'barbershop/pages/home.html')
