a43a51fbe0e33d1492d2e78458ef8e5aa54b9f6d
Abner J Pelisser <abner.pelisser@gmail.com>
2022-06-07 03:43:02 -0300

    inclusao da tela agendamento e consulta-agenda

--- barbershop/urls.py
+++ barbershop/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('galeria/', views.galeria, name='galeria'),
     path('catalogo/', views.catalogo, name='catalogo'),
+    path('consulta-agenda/', views.consulta_agenda, name='consulta_agenda'),
     path('agendamento/', views.agendamento, name='agendamento'),
 ]
--- barbershop/views.py
+++ barbershop/views.py
@@ -2,20 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'barbershop/pages/home.html')
+    context = {
+        'home_active': 'active',
+    }
+    return render(request, 'barbershop/pages/home.html', context)
 
 def galeria(request):
-    return render(request, 'barbershop/pages/galeria.html')
+    context = {
+        'galeria_active': 'active',
+    }
+    return render(request, 'barbershop/pages/galeria.html', context)
 
 def catalogo(request):
-    return render(request, 'barbershop/pages/catalogo.html')
+    context = {
+        'catalogo_active': 'active',
+    }
+    return render(request, 'barbershop/pages/catalogo.html', context)
 
 def agendamento(request):
+    context = {
+        'agendamento': 'active',
+    }
+    return render(request, 'barbershop/pages/agendamento.html', context)
+
+def consulta_agenda(request):
     meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
     dias_mes = range(1, 32)
 
     context = {
+        'consulta_active': 'active',
         'meses': meses,
         'dias_do_mes': dias_mes,
     }
-    return render(request, 'barbershop/pages/agendamento.html', context)
+    return render(request, 'barbershop/pages/consulta_agenda.html', context)
