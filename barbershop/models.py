87216fe826a56f03e5fd9dca80ae7ae06f5270a3
Abner J Pelisser <abner.pelisser@gmail.com>
2022-06-19 14:45:36 -0300

    ajustado servicos, removido as migracoes e criado as migracoes novamente

--- barbershop/models.py
+++ barbershop/models.py
@@ -1,11 +1,13 @@
 from django.db import models
-from datetime import time
+from datetime import time, datetime
 # Create your models here.
 
 
 class Cliente(models.Model):
+    data_hora_criacao = models.DateTimeField(auto_now_add=True)
     telefone_celular = models.CharField(max_length=14)
     nome = models.CharField(max_length=70)
+    data_hora_alteracao = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return (f'{self.telefone_celular} | {self.nome}')
@@ -13,19 +15,19 @@ class Cliente(models.Model):
 
 class Servico(models.Model):
     nome = models.CharField(max_length=70)
-    tempo_estimado = models.TimeField(default=time(0, 30))
     descricao = models.CharField(max_length=200)
+    tempo_estimado = models.TimeField(default=time(0, 30))
     ativo = models.BooleanField(default=True)
+    data_hora_criacao = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.nome
 
 class Agenda(models.Model):
-    data_criacao = models.DateTimeField(auto_now_add=True)
-    data_agenda = models.DateTimeField()
-    hora_inicio = models.TimeField()
-    hora_fim = models.TimeField()
-    confirmado = models.BooleanField(default=False)
+    data_hora_criacao = models.DateTimeField(auto_now_add=True)
+    data_hora_inicio = models.DateTimeField()
+    data_hora_fim = models.DateTimeField()
+    data_hora_confirmacao = models.DateTimeField(null=True)
     cliente = models.ForeignKey(Cliente, on_delete=models.RESTRICT, null=False)
     servico = models.ForeignKey(Servico, on_delete=models.RESTRICT, null=False)
 
