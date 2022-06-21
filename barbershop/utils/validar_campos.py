ca20ecdb1e9af633323148c0671cc69c5678e823
Abner J Pelisser <abner.pelisser@gmail.com>
2022-06-21 02:24:28 -0300

    implementando fluxo para agendamento

--- barbershop/utils/validar_campos.py
+++ barbershop/utils/validar_campos.py
@@ -19,9 +19,10 @@ def str_to_int(value):
     return valor_convertido
 
 
-def valida_campos(data, servico_id):
-    data_testada = str_to_datetime(data)
-    id_testado = str_to_int(servico_id)
-    valido = data_testada is not None and id_testado is not None
-
-    return valido, data_testada, id_testado
+def str_to_time(value):
+    valor_convertido = None
+    try:
+        valor_convertido = time.fromisoformat(value)
+    except:
+        ...
+    return valor_convertido
--- barbershop/views.py
+++ barbershop/views.py
@@ -2,10 +2,14 @@ import requests
 from django.shortcuts import render, redirect
 from .utils.twilio_util import send_whatsapp_message, send_sms_message
 from .utils.temporary import obter_servicos, obter_agenda, obter_horarios, obter_servico_pelo_id
-from .utils.validar_campos import valida_campos, str_to_datetime, str_to_int
+from .utils.validar_campos import str_to_datetime, str_to_int, str_to_time
 from django.http import Http404
 from datetime import date, datetime
 
+
+def verify_post_request(request):
+    if not request.POST:
+        raise Http404()
 
 def home(request):
     context = {
@@ -31,56 +35,135 @@ def catalogo(request):
 def inicio_agendamento(request):
     data_selecionada = date.today().isoformat()
 
+    dados_agendamento = request.session.get('dados_agendamento')
+    if dados_agendamento is not None:
+        del request.session['dados_agendamento']
+
+    erros = request.session.get('erros')
+    if erros is not None:
+        del request.session['erros']
+
     context = {
         'consulta_active': 'active',
         'lista_servicos': obter_servicos(),
         'data_selecionada': data_selecionada,
+        'erros': erros,
     }
     return render(request, 'barbershop/pages/inicio_agendamento.html', context)
 
 
 def consulta_agenda(request):
-    if not request.POST:
-        raise Http404()
-    data_informada = request.POST.get('data_agendamento')
-    servico_selecionado = request.POST.get('servico')
+    verify_post_request(request)
 
-    sao_campos_validos, data_convertida, servico_id = valida_campos(data_informada, servico_selecionado)
-    if sao_campos_validos:
-        servico = obter_servico_pelo_id(servico_id)
+    erros = []
+    data_convertida = str_to_datetime(request.POST.get('data_agendamento'))
+    servico_id = str_to_int(request.POST.get('servico'))
 
-    sao_campos_validos = sao_campos_validos and len(servico) > 0
+    if data_convertida is None:
+        erros.append("A data não é válida")
 
-    if not sao_campos_validos:
+    servicos = []
+    if servico_id is None:
+        erros.append("O serviço selecionado não é válido")
+    elif not len(erros) > 0:
+        servicos = obter_servico_pelo_id(servico_id)
+        if not len(servicos) > 0:
+            erros.append("Não foi possível carregar o serviço selecionado")
+
+    if len(erros) > 0:
+        request.session['erros'] = erros
         return redirect('barbershop:inicio_agendamento')
 
     horarios = obter_horarios()
+
+    request.session['dados_agendamento'] = {
+        'data': data_convertida.date().isoformat(),
+        'servico_id': servicos[0]['id'],
+        'nome_servico': servicos[0]['nome'],
+    }
 
     context = {
         'consulta_active': 'active',
         'lista_servicos': obter_servicos(),
         'data_selecionada': data_convertida.date().isoformat(),
-        'servico_selecionado': servico[0],
+        'servico_selecionado': servicos[0],
         'horarios': horarios,
     }
     return render(request, 'barbershop/pages/inicio_agendamento.html', context)
-    # return redirect('barbershop:agendamento')
 
 
-def agendamento(request):
+def pre_agendamento(request):
+    verify_post_request(request)
+    erros = []
+
+    dados_agendamento = request.session.get('dados_agendamento')
+    if dados_agendamento is None:
+        erros.append("Não foi possível recuperar os dados informados")
+
+    horario_selecionado = str_to_time(request.POST.get('horario_selecionado'))
+
+    if horario_selecionado is None:
+        erros.append("O horário selecionado não é válido")
+
+    if len(erros) > 0:
+        request.session['erros'] = erros
+        return redirect('barbershop:inicio_agendamento')
+
+    dados_agendamento['hora_agendada'] = horario_selecionado.isoformat()
+    request.session['dados_agendamento'] = dados_agendamento
+
+    print(dados_agendamento)
+
     context = {
         'consulta_active': 'active',
+        'dados': dados_agendamento,
+    }
+    return render(request, 'barbershop/pages/agendamento.html', context)
+
+def agendamento(request):
+    erros = request.session.get('erros')
+    if erros is not None:
+        del request.session['erros']
+
+    if erros is None:
+        verify_post_request(request)
+
+    context = {
+        'consulta_active': 'active',
+        'erros': erros,
     }
     return render(request, 'barbershop/pages/agendamento.html', context)
 
 
 def confirmar_agendamento(request):
-    if not request.POST:
-        raise Http404()
+    verify_post_request(request)
+
+    erros = []
+    dados_agendamento = request.session.get('dados_agendamento')
+    if dados_agendamento is None:
+        erros.append("Não foi possível recuperar os dados informados nas telas anteriores")
 
     nome_cliente = request.POST.get('nome_cliente')
     telefone_celular = request.POST.get('telefone_celular')
     observacao_agendamento = request.POST.get('observacao')
+
+    try:
+        if len(nome_cliente) <= 3:
+            erros.append("O nome deve conter pelo menos 3 caracteres")
+    except:
+        erros.append("O nome é obrigatório")
+
+    # TODO :: validar celular
+
+    if len(erros) > 0:
+        request.session['erros'] = erros
+        return redirect('barbershop:agendamento')
+
+    dados_agendamento['nome_cliente'] = nome_cliente
+    dados_agendamento['celular_cliente'] = telefone_celular
+    dados_agendamento['observacao_cliente'] = observacao_agendamento
+
+    del request.session['dados_agendamento']
 
     # envia mensagem - whatsapp
     # sid = send_whatsapp_message(
@@ -95,8 +178,18 @@ def confirmar_agendamento(request):
     # )
     # print(sid)
 
-    return redirect('barbershop:fim_agendamento')
+    context = {
+        'consulta_active': 'active',
+        'dados': dados_agendamento,
+    }
+
+    print(dados_agendamento)
+    return render(request, 'barbershop/pages/fim_agendamento.html', context)
 
 
 def fim_agendamento(request):
+    dados_agendamento = request.session.get('dados_agendamento')
+    if dados_agendamento is not None:
+        del request.session['dados_agendamento']
+
     return render(request, 'barbershop/pages/fim_agendamento.html')
